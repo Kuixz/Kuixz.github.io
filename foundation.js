ebc8bd08ef2c11084c140b7e6e7f60032dc3afa2
not set <notset@notset.com>
2024-03-04 22:29:07 +0000

    Barrel 1.0

--- foundation.js
+++ foundation.js
@@ -1,16 +1,37 @@
+const setAttributes = (node, attrs) => { 
+    for (const [attr, value] of Object.entries(attrs)) { 
+        switch (attr) {
+            case "parent": value.appendChild(node);  break;
+            default: node.setAttribute(attr, value); break;
+        }
+    }; 
+    return node 
+}
 
-// let hpi = Math.PI / 2
+const root = document.querySelector(":root");
+var bucketWidth = 144
+var bucketHeight = 120
+root.style.setProperty("--bucket-width", bucketWidth)
+root.style.setProperty("--bucket-height", bucketHeight)
 
-
+const links = {
+    "mikoban" : "https://kuixz.itch.io/mikoban",
+    "snowsweeper" : "https://github.com/Kuixz/snowsweeper",
+    "cellulart" : "https://chromewebstore.google.com/u/1/detail/pjeenahidnpjaajbiidagnackjdhnlam"
+}
+// let data = [
+//     [],[],[],[],[]
+// ]
 
 function createHoverElement(obj) {
     var img = new Image()
     img.src = "icons/" + obj + ".png"
     img.classList.add("hoverable")
     img.classList.add("tray")
-    // img.onload = function() {
-
-    // }
+    img.onclick = function() {
+        // t.textContent = obj
+        window.open(links[obj])
+    }
     img.onmouseenter = function() {
         // t.textContent = "hover"
         img.classList.add("hover")
@@ -24,55 +45,186 @@ function createHoverElement(obj) {
     // t.textContent = "c"
     return img
 }
-class HoverBucket {
-    // theta = 0
-    sprite = undefined
-    x = 0
 
-    constructor(obj, x = 0) {
-        this.sprite = createHoverElement(obj)
-        this.x = x
-        // t.textContent = parent
-        // parent.appendChild(this.sprite)
-        // this.shelf = shelf
-    }
-    offset(theta) {
-        this.sprite.style.left = (320 + this.x) + (0.1 * this.x * (Math.cos(theta) - 1)) + "px"
+class HoverIcon {
+    element
+
+    constructor(id, transformxpercent=0, transformypercent=0, size=1, rotdeg=0) {
+        var element = setAttributes(document.createElement("img"), { src:`icons/${id}.png`, class:"hoverable tray", 
+            style:`transform: scale(${size}) translate(${transformxpercent}%,${transformypercent}%) rotate(${rotdeg}deg);` })
+        element.onclick = function() {
+            // t.textContent = obj
+            window.open(links[id])
+        }
+        element.onmouseenter = function() {
+            // t.textContent = "hover"
+            element.classList.add("hover")
+        }
+        element.onmouseleave = function() {
+            // t.textContent = "unhover"
+            element.classList.remove("hover")
+        }
+
+        this.element = element
     }
 }
-class HoverShelf {
-    container = undefined
-    index
-    sprites = []
 
-    constructor(arr, index) {
-        var container = document.createElement("div")
-        barrel.appendChild(container)
-        container.classList = "hover-container"
-        arr.map((obj) => { 
-            var next = new HoverBucket(...obj) 
-            // t.textContent = next.sprite
-            container.appendChild(next.sprite)
-            next.sprite.onclick = (e) => {
-                t.textContent = this.container.style.zIndex
+class Bucket { 
+    element
+    
+    constructor(items) {
+        this.element = setAttributes(document.createElement("div"), { class:"bucket" })
+        var wallBack = new Image(); wallBack.src = "assets/back.png"; wallBack.classList.add("wall-back"); this.element.appendChild(wallBack)
+        for (const item of items) {
+            // if (item === undefined) {
+
+            // } else
+            if (item instanceof Array) {
+                var newIcon = new HoverIcon(...item)
+                this.element.appendChild(newIcon.element)
             }
-            this.sprites.push(next)
-        })
-        this.container = container
-        this.index = index
+            else if (item instanceof HoverIcon) {
+                this.element.appendChild(item.element)
+            }
+        }
+        var wallFront = new Image(); wallFront.src = "assets/front.png"; wallFront.classList.add("wall-front"); this.element.appendChild(wallFront)
+    }
+}
+
+class Shelf {  // TODO switch to using flex and justify center; support 1-bucket shelves
+    element = undefined
+    // index = 0
+    // axis
+    child_count
+    children = []
+    // sprites = []
+    // spacer
+
+    constructor(width, buckets) {
+        var coreWidth = width - bucketWidth
+        this.element = setAttributes(document.createElement("div"), { class:"hover-shelf" })
+        this.child_count = buckets.length
+        this.axis = coreWidth / 2
+        // this.spacer = coreWidth / (this.child_count - 1)
+
+        if (this.child_count == 1) { this.element.style.justifyContent = "center" }
+        for (const bucket of buckets) {
+            // var bucket = buckets[i]
+            if (bucket === undefined) {
+                var newBucket = new Bucket([])
+                this.children.push(newBucket)
+                this.element.appendChild(newBucket.element)
+            }
+            else if (bucket instanceof Array) {
+                var newBucket = new Bucket(bucket)
+                this.children.push(newBucket)
+                this.element.appendChild(newBucket.element)
+            }
+            else if (bucket instanceof Bucket) {
+                this.children.push(buckets[i])
+                this.element.appendChild(buckets[i].element)
+            }
+        }
     }
     rotate(theta) {
-        this.local_rotate(theta + this.index * 2 * segment)
+        var offset = (4 * (Math.cos(theta) - 1))
+        this.element.style.width = 100 + (2 * offset) + "%"
+        this.element.style.left = - offset + "%"
     }
-    local_rotate(theta) {
-        this.container.style.top = axis + 100 * Math.sin(theta) + "px"
-        var layer = Math.round((theta / segment) % (2 * shelves))
-        var zindex = shelves - Math.min(layer, 2 * shelves - layer)
-        this.container.style.zIndex = zindex
+}
 
-        for (const sprite of this.sprites) {
-            sprite.offset(theta)
+class Barrel {
+    element
+    childCount
+    children = []
+
+    axis
+    segment
+    diameter
+
+    constructor(width, height, shelves) {
+        // var coreWidth = width - bucketWidth
+        var coreHeight = height - bucketHeight
+        this.element = setAttributes(document.createElement("div"), { "style":`width:${width}; height:${height}`, "class":"barrel" })
+        this.childCount = shelves.length
+        this.axis = height / 2 - bucketHeight / 2
+        // this.children = shelves
+        this.diameter = coreHeight / 2
+        this.segment = (2 * Math.PI) / (2 * this.childCount)
+
+        setAttributes(document.createElement("div"), { style:`z-index:${this.childCount}`, class:"fade", parent: this.element })
+        setAttributes(document.createElement("div"), { style:`z-index:${Math.ceil(this.childCount/2)}`, class:"inner-fade", parent: this.element })
+
+        for (const shelf of shelves) {
+            if (shelf === undefined) {
+                var newShelf = new Shelf(0, [])
+                this.children.push(newShelf)
+                this.element.appendChild(newShelf.element)
+            } else if (shelf instanceof Array) {
+                var newShelf = new Shelf(width, shelf)
+                this.children.push(newShelf)
+                this.element.appendChild(newShelf.element)
+            }
+            else if (shelf instanceof Shelf) {
+                this.children.push(shelf)
+                this.element.appendChild(shelf.element)
+            }
         }
-        // this.container.textContent = zindex
     }
+    rotate(theta) {
+        for (var i = 0; i < this.childCount; i ++) {
+            const shelf = this.children[i]
+            const localTheta = theta + 2 * i * this.segment
+
+            shelf.element.style.top = this.axis + this.diameter * Math.sin(localTheta) + "px"
+ 
+            var layer = Math.round((localTheta / this.segment) % (2 * this.childCount))
+            var zindex = this.childCount - Math.min(layer, 2 * this.childCount - layer)
+            shelf.element.style.zIndex = zindex
+            // t.textContent = "3"
+
+            shelf.rotate(localTheta)
+        }
+    }
+}
+
+class WheelBarrel extends Barrel {
+    zero = 0
+    theta = 0
+    baseTheta = 0
+    targetTheta = 0
+    diff = 0
+    duration = 400;
+
+    constructor(...args) {
+        // this.firstFrame = this.firstFrame.bind(this)
+        // this.shift = this.shift.bind(this)
+        super(...args)
+    }
+    scroll(direction) {
+        if (direction == "down") { 
+            this.targetTheta = this.targetTheta + 2 * this.segment
+        } else if (direction == "up") { 
+            this.targetTheta = this.targetTheta - 2 * this.segment
+        }
+        this.baseTheta = this.theta
+        this.diff = this.targetTheta - this.theta
+        window.requestAnimationFrame(this.firstFrame);
+    }
+    firstFrame = function (timeStamp) {
+        // t.textContent = timeStamp
+        // t.textContent = this
+        this.zero = timeStamp;
+        this.shift(timeStamp);
+    }.bind(this)
+    shift = function(timeStamp) {
+        const value = (timeStamp - this.zero) / this.duration
+        this.theta = this.baseTheta + value * (this.targetTheta - this.baseTheta)
+        if (value < 1) {
+            this.rotate(this.theta)
+            requestAnimationFrame((t) => this.shift(t));
+        } else {
+            this.rotate(this.targetTheta)
+        }
+    }.bind(this)
 }
--- index.js
+++ index.js
@@ -1,102 +1,40 @@
-// document.body.style.color = "blue"
-let axis = 100
 
-var t = document.getElementById("caption")
 var frame = document.getElementById("frame")
-var barrel = document.getElementById("barrel")
-var fade = (function(){
-    var f = document.createElement("div")
-    f.id = "fade"
-    return f
-    // return 1
-})()
-var innerFade = (function(){
-    var f = document.createElement("div")
-    f.id = "inner-fade"
-    return f
-    // return 1
-})()
 
-let shelves = 5
-let segment = (2 * Math.PI) / (2 * shelves)
 
-barrel.appendChild(fade)
-fade.style.zIndex = shelves
-
-barrel.appendChild(innerFade)
-innerFade.style.zIndex = Math.ceil(shelves / 2)
-// var innerFade = 
-// t.textContent = fade
-// document.onerror = function(e) {
-//     t.textContent = e
-// }
-
-var layer0 = new HoverShelf([["flag", -100], ["mikoban", 100]], 0)
+var item1 = new HoverIcon("snowsweeper", 10, 10, 0.7, -3)
+var item2 = new HoverIcon("mikoban", 60, 0, 0.7, 10)
+var layer0 = new Shelf(740, [[item1],,[item2],,])
+// layer0 = [[item1],[item2]]
+// var layer1 = new Shelf(740, [1,2,3])
+// var layer2 = new Shelf(740, [1,2,3,4])
+// var layer3 = new Shelf(740, [1,2,3])
+// var layer4 = new Shelf(740, [1,2])
 // var layer0 = new HoverShelf([["flag", -320], ["flag", 0], ["flag", 320]], 0)
 // var layer1 = new HoverShelf([["mikoban", -320], ["mikoban", 0], ["mikoban", 320]], 1)
 // var layer2 = new HoverShelf([["flag", -320], ["flag", 0], ["flag", 320]], 2)
 // var layer3 = new HoverShelf([["mikoban", -320], ["mikoban", 0], ["mikoban", 320]], 3)
 // var layer4 = new HoverShelf([["flag", -320], ["flag", 0], ["flag", 320]], 4)
 
-var layers = [layer0]
+var layers = [layer0,[,,,],[,,,,],[,,,],[,,,,]]
+// layers = [layer0,[,]]
+// var layers = [layer0,[[["snowsweeper", 10, 10, 0.7, -3]],,[["snowsweeper", 10, 10, 0.7, -3]],,],]
 // var layers = [layer0, layer1, layer2]
 // var layers = [layer0, layer1, layer2, layer3, layer4]
 
-
-var duration = 400;
-var zero = 0
-var theta = 0
-var baseTheta = 0
-var targetTheta = 0
-var diff = 0
+// t.textContent = "2"
+var barrel = new WheelBarrel(740, 420, layers)
+// document.body.appendChild(barrel.element)
+frame.appendChild(barrel.element)
+barrel.rotate(0)
+// t.textContent = "4"
 
 document.addEventListener("keydown", (e) => {
     // t.textContent += ` ${e.code}`;
-    if (!['KeyS', 'ArrowDown'].includes(e.code)) { return }
-    // start = timeStamp;
-    targetTheta = targetTheta + 2 * segment
-    // zero = timeStamp
-    baseTheta = theta
-    diff = targetTheta - theta
-    // if (animating) { return }
-    // animating = true
-    // shift(timeStamp)
-    window.requestAnimationFrame(firstFrame);
-});
-
-
-
-// var animating = false
-// // requestAnimationFrame(firstFrame);
-function firstFrame(timeStamp) {
-    zero = timeStamp;
-    shift(timeStamp);
-}
-function shift(timeStamp) {
-    const value = (timeStamp - zero) / duration
-    theta = baseTheta + value * (targetTheta - baseTheta)
-    if (value < 1) {
-        // t.textContent = theta
-        for (const layer of layers) {
-            layer.rotate(theta)
-        }
-        requestAnimationFrame((t) => shift(t));
-    } // else element.style.opacity = 1; 
-    else {
-        for (const layer of layers) {
-            layer.rotate(targetTheta)
-        }
+    if (['KeyS', 'ArrowDown'].includes(e.code)) { 
+        barrel.scroll("down")
     }
-}
-  
-// t.textContent = "ccc"
-// document.addEventListener("scroll", (event) => {
-//     t.textContent = window.scrollY
-// });
-
-// onscroll = (event) => {};
-
-// document.body.appendChild(g)
-// document.body.style.color = white
-
-// Lets make it WALL E!
+    if (['KeyW', 'ArrowUp'].includes(e.code)) { 
+        barrel.scroll("up")
+    }
+});
